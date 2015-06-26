2bd5a49c76cface694a9227f76041b91dc8278c4
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-26 14:32:20 +0200

    Default port 8080

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(server.router.routes());
 const koaServer = http.Server(app.callback());
 server.socketServer(addon.csEvents, koaServer);
 
-const port = process.env.port || 3000;
-koaServer.listen(3000);
+const port = process.env.port || 8080;
+koaServer.listen(port);
 console.log('Server listening on port ' + port);
