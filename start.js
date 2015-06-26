550474cdccbc414f539081edfd4ff189796057db
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-26 19:32:19 +0200

    New start script

--- start.js
+++ start.js
@@ -1,3 +1,38 @@
-var forever = require('forever-monitor');
+var cluster = require('cluster'),
+    stopSignals = [
+        'SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+        'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+    ],
+    stopping = false,
+    env = process.env,
+    i;
 
-forever.start('app.js');
+//const workers = env.NODE_CLUSTER_WORKERS;
+var workers = 1;
+
+cluster.on('disconnect', function(worker) {
+    if (env.NODE_ENV == 'production') {
+        if (!stopping) {
+            cluster.fork();
+        }
+    } else {
+        process.exit(1);
+    }
+});
+if (cluster.isMaster) {
+    console.log('Starting %d workers...', workers || 4);
+    for (i = 0; i < (workers || 4); i++) {
+        cluster.fork();
+    }
+    if (env.NODE_ENV == 'production') {
+        stopSignals.forEach(function(signal) {
+            process.on(signal, function() {
+                console.log('Got %s, stopping workers...', signal);
+                stopping = true;
+                cluster.disconnect();
+            });
+        });
+    }
+} else {
+    require('./app.js');
+}
